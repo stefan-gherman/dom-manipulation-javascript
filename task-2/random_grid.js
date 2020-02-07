8b8d32dcc470981a628df39a1770bd8065529842
Bence <bence@seedr.co.uk>
2020-02-07 10:51:38 +0100

    Initial commit

---
+++ task-2/random_grid.js
@@ -0,0 +1,5 @@
+function main() {
+    // event handlers and other code here
+}
+
+main();
---
+++ task-3/move_square.js
@@ -0,0 +1,15 @@
+// use these keycodes for calling the appropriate functions
+// write your moveX functions and after that
+// you can call these like moves[keycode]();
+const moves = {
+    '97': moveLeft,
+    '115': moveDown,
+    '119': moveUp,
+    '100': moveRight
+};
+
+function main() {
+    // event handlers and other code here
+}
+
+main();
