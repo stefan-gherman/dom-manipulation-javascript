2a70c65340a81b7fc3b3d83706fa1389cc8279d4
Stefan Gherman <scgheman@gmail.com>
2020-02-10 04:59:03 +0200

    Add solution for random grid exercise.

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,5 +1,34 @@
 function main() {
-    // event handlers and other code here
+
+
+    const gridHolder = document.querySelector('.container');
+
+    const brAdder = document.createElement('BR');
+    for (let i = 0; i< 10; i++){
+        for (let j = 0; j< 10; j++){
+            const divInner = document.createElement('DIV');
+            divInner.setAttribute('data-pos-row',String(i));
+            divInner.setAttribute('data-pos-col', String(j));
+            divInner.setAttribute("class", 'cell');
+            divInner.innerText = 'DIV' + ' ' + i + ' ' +j;
+            gridHolder.appendChild(divInner);
+        }
+        gridHolder.appendChild(brAdder);
+    }
+
+
+    let cellsArray = document.querySelectorAll('.cell');
+
+    setInterval(function(){
+       let randomGrid = Math.floor(Math.random() * 100);
+       let currentCell = cellsArray[randomGrid];
+       currentCell.setAttribute("class","cell active");
+
+       setTimeout(function(){
+           currentCell.setAttribute("class", "cell");
+       }, 250)
+    }, 250)
+
 }
 
 main();
